cf2658d082a58175c3f45e394b798f338474a023
methkal <methkal.khalawi.7e2@itb.cat>
2019-07-04 19:06:40 +0200

    update menu-item component to navigate by using withRouter()

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
 
-import HomePage from './pages/homepage/homepage.component';
+import "./App.css";
 
-function App() {
+import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => <div>hat page</div>;
+
+const App= () => {
   return (
     <div>
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/shop/hats" component={HatsPage} />
     </div>
   );
 }
