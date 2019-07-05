d0235ebce3e8053ad7368f4685d8686de70a456c
methkal <methkal.khalawi.7e2@itb.cat>
2019-07-05 16:01:43 +0200

    creating and styling header component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,30 +2,26 @@ import React from "react";
 
 import SHOP_DATA from "./shop.data.js";
 
-import PreviewCollection from '../../components/preview-collection/preview-collection.component';
+import PreviewCollection from "../../components/preview-collection/preview-collection.component";
 
 class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
 
     this.state = {
       collections: SHOP_DATA,
     };
   }
-  
-  render(){
-      const {collections} = this.state;
-      return (
 
+  render() {
+    const { collections } = this.state;
+    return (
       <div className="shop-page">
-          {
-
-        collections.map(({id,...otherCollectionProps}) => (
-            <PreviewCollection key={id} {...otherCollectionProps}/>
-        ))
-          }
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <PreviewCollection key={id} {...otherCollectionProps} />
+        ))}
       </div>
-      )
+    );
   }
 }
 
