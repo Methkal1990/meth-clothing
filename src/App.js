65063a1fdbf5b7547301a30a697fd54b8ec3cd0e
methkal <methkal.khalawi.7e2@itb.cat>
2019-07-08 03:21:10 +0200

    implementing the ability for a costum sign up and to store authenticated users in firestore database

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInSignUpPage from "./pages/signin-signup/signin-signup.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -20,8 +20,24 @@ class App extends React.Component {
 
   unSubscribeFromAuth = null;
   componentDidMount() {
-    this.unSubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unSubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+        });
+
+        console.log(this.state);
+        
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
   componentWillUnmount() {
@@ -30,7 +46,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
--- src/pages/signin-signup/signin-signup.component.jsx
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import './signin-signup.styles.scss';
 
 import SignIn from '../../components/signin/signin.component';
+import SignUp from '../../components/signup/signup.component';
 
 const SignInSignUpPage = () => (
     <div className="signin-signup">
         <SignIn />
+        <SignUp />
     </div>
 )
 
