4664ab7a3bfd916635c031b03b2f9f84a4671ae3
methkal <methkal.khalawi.7e2@itb.cat>
2019-07-11 01:22:44 +0200

    update cart icon and cart dropdown by using selectors for performence purposes ( the concept memoization or caching)

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,26 +1,25 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import Custombutton from "../custom-button/custom-button.component";
+import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 
 import "./cart-dropdown.styles.scss";
 
-const CartDropDown = ({cartItems}) => (
+const CartDropDown = ({ cartItems }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
-      {
-        cartItems.map(cartItem => (
-          <CartItem key={cartItem.id} item={cartItem} />
-        ))
-      }
+      {cartItems.map(cartItem => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
     </div>
-    <Custombutton>Go To Checkout</Custombutton>
+    <CustomButton>Go To Checkout</CustomButton>
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state),
 });
 
 export default connect(mapStateToProps)(CartDropDown);
