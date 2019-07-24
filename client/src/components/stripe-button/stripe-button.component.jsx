898326e8481e6a1d16977758f847252f66d5039a
methkal <methkal.khalawi.7e2@itb.cat>
2019-07-24 16:00:33 +0200

    update stripe integration

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_dXPm70zSAsqPUYc2Qc02gAZG0047lLAo6i";
+
+  const onToken = token => {
+      console.log(token);
+      alert('Payment Successful')
+  }
+
+  return(
+      <StripeCheckout 
+      label='Pay Now'
+      name = 'Meth Clothing'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+      />
+  )
+
+};
+
+export default StripeCheckoutButton;
